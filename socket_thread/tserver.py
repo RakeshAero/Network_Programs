905de9fdc14fdda3b2e482181d49a2dd736604a3
RakeshAero <rakeshaero55743@gmail.com>
2025-05-21 08:34:50 -0700

    Bending: Exception,Subprocess

--- socket_thread/tserver.py
+++ socket_thread/tserver.py
@@ -1,17 +1,40 @@
 import socket
 import threading
 
-def processThread(s):
-    return True
+def thread_program(conn,addr):
+
+    print("Connected to : ",addr)
+    message = "Request in Progress please wait"
+    conn.send(message.encode())
+
+    # while message.lower().strip() != "exit":
+    for i in range(4):
+        conn.send(message.encode())
+        data = conn.recv(1024).decode()
+        if not data:
+            print("No messages Received")
+    
+        print("Client => ",data)
+        # message = input("Server => ")
+    conn.close()
 
 
-def connection():
+
+def server_program():
     host = '127.0.0.1'
     port = 4444
-    while True:
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.bind((host,port))
-        s.listen(4)
+   
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind((host,port))
+    s.listen(4)
 
+    print("Server Listening on Port => {}".format(port))
+
+    while True:
         conn,addr = s.accept()
-        client_server = threading.Thread(target=processThread,args=(conn,))
+        client_server = threading.Thread(target=thread_program,args=(conn,addr,))
+        client_server.start()
+
+
+if __name__ == "__main__":
+    server_program()
---
+++ subprocess/Pserver.py
@@ -0,0 +1,5 @@
+import socket
+import threading
+import subprocess
+
+p = subprocess.Popen()
